8ba34ee65f7ed1096687283249cabe6436687699
Vlad Polevoi <vlad.polewoi1@gmail.com>
2024-04-11 16:55:45 +0300

    Initial Commit

---
+++ SwiftUIInPractice/SwiftUIInPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIInPracticeApp.swift
+//  SwiftUIInPractice
+//
+//  Created by Quest76 on 11.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIInPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
